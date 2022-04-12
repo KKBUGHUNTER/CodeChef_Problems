dbc59a68d2fc1487ec4331b941570d4977b1c9b1
KK BUG HUNTER <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-04-12 15:06:11 +0000

    Add files via upload

---
+++ 8) Which Mixture.py
@@ -0,0 +1,11 @@
+def WhichMixture():
+    for _ in range(int(input())):
+        a, b =map(int, input().split())
+        if a == 0:
+            print("Liquid")
+        elif b==0:
+            print("Solid")
+        else: 
+            print("Solution")
+WhichMixture()
+
