6aa0e51994427ef18854952c11e7b2fa7fd759c4
KK BUG HUNTER <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-04-12 17:51:18 +0000

    Add files via upload

---
+++ 13) Ezio and Guards.py
@@ -0,0 +1,10 @@
+def Ezio_and_Guards():
+    for _ in range(int(input())):
+        x, y =map(int, input().split())
+        if x < y :
+            print('NO')
+        elif x ==y:
+            print('YES')
+        else:
+            print('YES')
+Ezio_and_Guards()
