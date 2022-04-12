f6e6125dfbf5dc602bc0a38ce4cabd4c2bf92c9a
KK BUG HUNTER <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-04-12 15:51:50 +0000

    Add files via upload

---
+++ 10) Body Mass Index.py
@@ -0,0 +1,13 @@
+def Body_Mass_Index():
+    for _ in range(int(input())):
+        h, m = map(int, input().split())
+        bmi=h/(m*m)
+        if bmi <= 18:
+            print("1")
+        elif bmi <= 24:
+            print("2")
+        elif bmi <= 29:
+            print("3")
+        else:
+            print('4')
+Body_Mass_Index()
